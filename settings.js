33f0ee16bcd07053b56c12a85a46fe9a4abb0802
earthsaf <154933783+earthsaf@users.noreply.github.com>
2024-08-27 01:51:36 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 global.prefa = ['','!','.',',','🐤','🗿']
 
-global.owner = ['628XXXX']
+global.owner = ['2347089532266']
 global.botname = 'Rxhl Bot'
 global.baileys1 = require('@whiskeysockets/baileys') 
 global.sticker1 = "Rxhl Official"
